e6162f23e8360eab184836743de967d4640b8184
aseel-sm <aseelsm0027@gmail.com>
2022-07-05 21:11:18 +0530

    updated project with tflite

--- app/src/main/java/com/example/har/MainActivity.java
+++ app/src/main/java/com/example/har/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.har;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -13,16 +12,41 @@ import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
 
+import org.tensorflow.lite.Interpreter;
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+
+
+
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.nio.ByteBuffer;
+
+import java.nio.ByteOrder;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 import java.util.Timer;
 
+
+
+import android.content.res.AssetFileDescriptor;
+
+
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+
+
+
 public class MainActivity extends AppCompatActivity implements SensorEventListener,TextToSpeech.OnInitListener {
 
     private static final int TIME_STAMP = 100;
+    private static final int FEATURES = 9;
     private static final String TAG = "MainActivity";
 
     private static List<Float> ax,ay,az;
@@ -33,7 +57,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private Sensor mAccelerometer, mGyroscope, mLinearAcceleration;
 
     private float[] results;
-    private ActivityClassifier classifier;
+
+    private int ttsFlag;
+ 
 
     private TextToSpeech textToSpeech;
 
@@ -41,6 +67,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     private Switch ClassifierSwitch;
     private Switch SoundSwitch;
+
+
+    static Interpreter interpreter;
+
 
     private static String[] labels = {"Biking","DownStairs", "Jogging", "Sitting", "Standing","Upstairs","Waliking",};
 
@@ -54,6 +84,14 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         ax=new ArrayList<>(); ay=new ArrayList<>(); az=new ArrayList<>();
         gx=new ArrayList<>(); gy=new ArrayList<>(); gz=new ArrayList<>();
         lx=new ArrayList<>(); ly=new ArrayList<>(); lz=new ArrayList<>();
+
+        try {
+            interpreter = new Interpreter(loadModelFile(),null);
+            Log.i(TAG, "Loaded Model1-----------------------------------: ");
+        } catch (IOException e) {
+            Log.i(TAG, "Loading Model failed-----------------------------------: ");
+            e.printStackTrace();
+        }
 
 
         ClassifierSwitch = (Switch) findViewById(R.id.classifierSwitch);
@@ -71,12 +109,22 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         });
 
 
+        SoundSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (!isChecked){
+//
+                    textToSpeech.stop();
+                }
+//
+            }
+        });
+
+
         mSensorManager=(SensorManager) getSystemService(SENSOR_SERVICE);
         mAccelerometer=mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         mGyroscope = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
         mLinearAcceleration = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
 
-        classifier=new ActivityClassifier(getApplicationContext());
 
         mSensorManager.registerListener(this,mAccelerometer, SensorManager.SENSOR_DELAY_FASTEST);
         mSensorManager.registerListener(this,mGyroscope, SensorManager.SENSOR_DELAY_FASTEST);
@@ -84,10 +132,26 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         textToSpeech = new TextToSpeech(this, this);
         textToSpeech.setLanguage(Locale.US);
-        textToSpeech.setSpeechRate(0.5f);
-        ClassifierSwitch.setChecked(true);
+        textToSpeech.setSpeechRate(0.7f);
+//        ClassifierSwitch.setChecked(true);
 
     }
+
+
+
+
+    private MappedByteBuffer loadModelFile() throws IOException
+    {
+        AssetFileDescriptor assetFileDescriptor = this.getAssets().openFd("model_cnnn.tflite");
+        FileInputStream fileInputStream = new FileInputStream(assetFileDescriptor.getFileDescriptor());
+        FileChannel fileChannel = fileInputStream.getChannel();
+
+        long startOffset = assetFileDescriptor.getStartOffset();
+        long len = assetFileDescriptor.getLength();
+
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY,startOffset,len);
+    }
+
 
     private void initLayoutItems() {
         bikingTextView = findViewById(R.id.biking_prob);
@@ -100,7 +164,28 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     }
 
 
+    static public float[][] predictProbabilities(float[] data) {
+        ByteBuffer input = ByteBuffer.allocateDirect(TIME_STAMP * 9 * 4).order(ByteOrder.nativeOrder());
+        float[][] result = new float[1][7];
+//        System.out.print("Data---------------------------------------");
+//        Log.i(TAG, "Data to feed: "+Arrays.toString(data));
+        for (int i=0;i<TIME_STAMP;i++){
+            input.putFloat(data[i]);
+        }
 
+        input.rewind();
+
+
+//      Log.i(TAG, "BUffer");
+//
+//        for (int i = 1; i <= 30; i++)
+//            Log.i(TAG, "Buffer: "+input.getFloat());
+//
+//        System.out.print("]");
+
+     interpreter.run(input,result);
+        return result;
+    }
 
 
     @Override
@@ -119,7 +204,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             ly.add(event.values[1]);
             lz.add(event.values[2]);
         }
-
+if(ClassifierSwitch.isChecked()==true)
         predictActivity();
     }
 
@@ -142,43 +227,50 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             data.addAll(ay.subList(0,TIME_STAMP));
             data.addAll(az.subList(0,TIME_STAMP));
 
-            data.addAll(gx.subList(0,TIME_STAMP));
-            data.addAll(gy.subList(0,TIME_STAMP));
-            data.addAll(gz.subList(0,TIME_STAMP));
+
 
             data.addAll(lx.subList(0,TIME_STAMP));
             data.addAll(ly.subList(0,TIME_STAMP));
             data.addAll(lz.subList(0,TIME_STAMP));
 
-            results = classifier.predictProbabilities(toFloatArray(data));
-            Log.i(TAG, "predictActivity: "+ Arrays.toString(results));
+            data.addAll(gx.subList(0,TIME_STAMP));
+            data.addAll(gy.subList(0,TIME_STAMP));
+            data.addAll(gz.subList(0,TIME_STAMP));
+            System.out.print("Data---------------------------------------");
+              results = predictProbabilities(toFloatArray(data))[0];
+    Log.i(TAG, "predictActivity: "+ Arrays.toString(results));
 
-            bikingTextView.setText(Float.toString(round(results[0],2)) );
-            downstairsTextView.setText( Float.toString(round(results[1],2)));
-            joggingTextView.setText(Float.toString( round(results[2],2)));
-            sittingTextView.setText(Float.toString( round(results[3],2)));
-            standingTextView.setText(Float.toString( round(results[4],2)));
-            upstairsTextView.setText(Float.toString( round(results[5],2)));;
-            walkingTextView.setText(Float.toString(round(results[6],2)));
+                bikingTextView.setText(Float.toString(round(results[0],2)) );
+                downstairsTextView.setText( Float.toString(round(results[1],2)));
+                joggingTextView.setText(Float.toString( round(results[2],2)));
+                sittingTextView.setText(Float.toString( round(results[3],2)));
+                standingTextView.setText(Float.toString( round(results[4],2)));
+                upstairsTextView.setText(Float.toString( round(results[5],2)));;
+                walkingTextView.setText(Float.toString(round(results[6],2)));
 
-            data.clear();
-            ax.clear(); ay.clear(); az.clear();
-            gx.clear(); gy.clear(); gz.clear();
-            lx.clear();ly.clear(); lz.clear();
+                data.clear();
+                ax.clear(); ay.clear(); az.clear();
+                gx.clear(); gy.clear(); gz.clear();
+                lx.clear();ly.clear(); lz.clear();
 
-
-
-            float max = -1;
-            int idx = -1;
-            for (int i = 0; i < results.length; i++) {
-                if (results[i] > max) {
-                    idx = i;
-                    max = results[i];
+                float max = -1;
+                int idx = -1;
+                for (int i = 0; i < results.length; i++) {
+                    if (results[i] > max) {
+                        idx = i;
+                        max = results[i];
+                    }
                 }
-            }
-            if(SoundSwitch.isChecked() == true){
-                textToSpeech.speak(labels[idx], TextToSpeech.QUEUE_ADD, null, null);
-            }
+                if(ttsFlag!=idx){
+                    textToSpeech.stop();
+                }
+                ttsFlag=idx;
+
+                if(SoundSwitch.isChecked() == true){
+                    textToSpeech.speak(labels[idx], TextToSpeech.QUEUE_ADD, null, null);
+
+                }
+            Log.i(TAG, "Prediction: "+ labels[idx]);
 
 
         }
@@ -191,11 +283,24 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     }
 
     private float[] toFloatArray(List<Float> data) {
+
         int i=0;
         float[] array=new float[data.size()];
         for (Float f:data) {
             array[i++] = (f != null ? f: Float.NaN);
         }
+//        System.out.println("ArraySize"+array.length);
+//        System.out.println("ArrayContent:"+Arrays.toString(array));
+
+
+//        int n=TIME_STAMP;
+//        int m=FEATURES;
+//        float[][] newArr=new float[n][m];
+//        for (i=0;i<n;i++){
+//            for (int j=0;j<m;m++){
+//                newArr[i][j]=array[(j*n)+i];
+//            }
+//        }
         return array;
     }
 
